ea5f80203f79576c53d8c9d6e9f5d8fba96603b8
malorie16 <malorie18@gmail.com>
2018-05-02 18:41:57 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  new_array = []
+  return new_array
+end
+
+def array_with_two_elements
+  array = ["bleep", "bloop"]
+  return array
+end
+
+def first_element(taylor_swift)
+  taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift[-1]
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end
+
+def length_of_array(programming_languages)
+  programming_languages.count
+end
